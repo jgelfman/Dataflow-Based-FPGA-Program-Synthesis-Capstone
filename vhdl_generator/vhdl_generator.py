e11387bf0524dde6218da83f9954a3d9dda38348
Jonathan G <gelfman.jonathan@u.yale-nus.edu.sg>
2021-03-08 10:11:13 +0800

    Adding XML parser + first file generator

---
+++ vhdl_generator/vhdl_generator.py
@@ -0,0 +1,75 @@
+# This is a VHDL code generator that uses ElementTree XML to take in XML files describing dataflow graphs compiled using FAUST, and in-return produces the corresponding entities and connections in VHDL to be simulated in Xilinx Vivado.
+
+import xml.etree.ElementTree as ET
+inputfile = input("Enter the exact file name (e.g. copy1.xml):")
+
+fileTree = ET.parse(inputfile)
+fileRoot = fileTree.getroot()
+applicationGraph = fileRoot[0]
+sdf = applicationGraph[0]
+
+# Get sdf name and type
+sdfList = list((sdf.attrib).items())
+sdfName = sdfList[0][1]
+sdfType = sdfList[1][1]
+
+# Generate list of actor names + types + ports
+actors = []
+for actor in sdf.iter('actor'):
+    listitem = list(actor.attrib.items())
+    actors.append(listitem)
+
+actorsList = []
+for i in range(len(actors)):
+    actorNumID = "Actor" + str(i+1)
+    actorName = actors[i][0][1]
+    actorType = actors[i][1][1]
+    actorPorts = []
+
+    # Generate list of port names + direction tuples
+    for port in sdf[i].iter('port'):
+            portName = port.get('name')
+            portType = port.get('type')
+            temp = (portName, portType)
+            actorPorts.append(temp)
+    temp = (actorNumID, actorType, actorName, actorPorts)
+    actorsList.append(temp)
+#(actorNumID, actorType, actorName, [(portName, direc), ...])
+
+# Generate list of signals between all ports
+signals = []
+for channel in sdf.iter('channel'):
+    listitem = list(channel.attrib.items())
+    signals.append(listitem)
+
+signalsList = []
+for i in range(len(signals)):
+    signalName = signals[i][0][1]
+    srcActor = signals[i][1][1]
+    srcPort = signals[i][2][1]
+    dstActor = signals[i][3][1]
+    dstPort = signals[i][4][1]
+    acts = (srcActor, dstActor)
+    prts = (srcPort, dstPort)
+    signal = [signalName, acts, prts]
+    signalsList.append(signal)
+#[signalName, (srcActor, dstActor), (srcPort, dstPort)]
+
+# Create entitiy VHDL file
+import vhdl_generator_node
+vhdl_generator_node.returnNode()
+
+#create buffer VHDL file
+
+
+#create entity connections based on above
+
+#create wrapper file connecting entities
+
+
+
+data = "test"
+
+output = open("output.txt", "w")
+output.write(str(data))
+output.close()
---
+++ vhdl_generator/vhdl_generator_node.py
@@ -0,0 +1,48 @@
+import os
+
+def returnNode():
+    node_import = str(
+        "library ieee; \n" + 
+        "use ieee.std_logic_1164.all; \n" +
+        "use ieee.numeric_std.all; \n" +
+        "\n"
+        )
+    node_entity = str(
+        "entity entity_node is \n" +
+        "port ( \n"
+        "        entity_clk : in std_logic; \n"
+        "        entity_rst : in std_logic; \n"
+        "\n" +
+        "        entity_in_ready : in std_logic; \n"
+        "        entity_out_ready : out std_logic; \n"
+        "\n" +
+        "        entity_in_valid : in std_logic; \n"
+        "        entity_out_valid : out std_logic; \n"
+        "\n" +
+        "        entity_in_opening : in std_logic_vector; \n"
+        "        entity_out_opening : out std_logic_vector \n"
+        "    );  \n"
+        "\n" +       
+        "end entity_node; \n"
+    )
+    node_arch = str(
+        "architecture copy1_arch of entity_node is \n" +
+        "\n" +
+        "    begin \n" +
+        "\n" +
+        "    entity_out_ready <= entity_in_ready; \n" +
+        "    entity_out_valid <= entity_in_valid; \n" +
+        "    entity_out_opening <= entity_in_opening; \n" +
+        "\n" +
+        "end architecture; \n"
+    )
+    
+    whole_node = node_import + "\n" + node_entity + "\n" + node_arch
+
+    # Add into a subdirectory output
+    os.makedirs("output")
+    output = open("./output/entity_node.vhdl", "w")
+    output.write(str(whole_node))
+    output.close()
+
+returnNode()
